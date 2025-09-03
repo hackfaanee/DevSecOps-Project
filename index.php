1d15e4743cf5a0e30301cc221a568a8995adb0ba
hackfaanee <fanihacker418@gmail.com>
2025-09-03 09:20:01 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+// index.php
+
+$title = "Digital Fortress";
+$message = "Access Granted. Welcome to the system.";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $title; ?></title>
+    <style>
+        body {
+            background-color: #000;
+            color: #0f0;
+            font-family: "Courier New", monospace;
+            text-align: center;
+            margin-top: 20%;
+        }
+        h1 {
+            font-size: 2.5em;
+            animation: blink 1s step-start infinite;
+        }
+        @keyframes blink {
+            50% { opacity: 0; }
+        }
+    </style>
+</head>
+<body>
+    <h1><?php echo $message; ?></h1>
+</body>
+</html>
